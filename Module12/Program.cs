9083b0c9f8722dcf8bd01f1a7ffcbe514140b46f
CS-SF-Kon <vladislav.konovalov@evraz.com>
2024-11-02 14:23:09 +0700

    Добавьте файлы проекта.

---
+++ Module12/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Module12;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<User> users = new() // пробую работу с коллекциями, очень удобно
+        {
+            new User("Петя", "PitRusha", true),
+            new User("Вася", "vasyan228", false),
+            new User("Игорь", "ChervIgor", true)
+        };
+
+        users.Add(new User("Kolya", "kalumbasik", false)); // почему "users.Add();" отображается тусклым шрифтом?
+
+        List<string> ads = new()
+        {
+            "сделать бочку. Скидка на бочки только в этом месяце.",
+            "мутировать в гидралиска. +10 к скорости передвижения.",
+            "оформить премку по цене чашки кофе, чтобы больше не видеть этот бред."
+        };
+
+        foreach (var user in users)
+        {
+            Console.WriteLine($"Здравствуйте, {user.Name}");
+            if (!user.IsPremium)
+            {
+                foreach (string adv in ads)
+                {
+                    Console.WriteLine($"{user.Name}, попробуйте {adv}");
+                }
+            } 
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Module12/User.cs
@@ -0,0 +1,35 @@
+﻿namespace Module12;
+
+/// <summary>
+/// Класс, описывающий пользователя
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// Имя пользователя
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Логин пользователя
+    /// </summary>
+    public string Login{ get; set; }
+
+    /// <summary>
+    /// Премиальный или нет
+    /// </summary>
+    public bool IsPremium { get; set; }
+
+    /// <summary>
+    /// Конструктор пользователя
+    /// </summary>
+    /// <param name="name">имя</param>
+    /// <param name="login">логин</param>
+    /// <param name="is_prem">отметка премиум</param>
+    public User(string name, string login, bool is_prem)
+    {
+        Name = name;
+        Login = login;
+        IsPremium = is_prem;
+    }
+}
